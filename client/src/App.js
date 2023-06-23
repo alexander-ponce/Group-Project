e704da856d1bc9992afc9e6ae6fa36b57d778768
alexander-ponce <alex@poncealex.com>
2023-06-23 10:23:21 -0700

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,19 @@ import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import Login from './components/Login';
 import Register from './components/Register';
+import NavBar from './components/NavBar';
 
 function App() {
 
-  const [user, setUser] = useState({})
+      // just for fun
+      const [title, setTitle] = useState('Site Header!')
+      // 1 ) CREATE A STATE TO SAVE THE USER
+      const [user, setUser] = useState({})
 
   return (
     <div className="App">
       <BrowserRouter>
+      <NavBar user={user} setUser={setUser}/>
         <Routes>
         <Route exact path ="/" element={<LandingPage  />} />
           <Route path='/login' element={<Login setUser={setUser}/>} />
---
+++ client/src/components/Checkout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Checkout = () => {
+  return (
+    <div>Checkout Cart</div>
+  )
+}
+
+export default Checkout
