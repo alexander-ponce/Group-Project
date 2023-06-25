bb0aa64297f9e357994f25435d8f0d553dbc9bd0
Tony Schock <tony.schock@gmail.com>
2023-06-25 14:36:41 -0600

    updated image size and added link to success page

--- client/src/components/Success.jsx
+++ client/src/components/Success.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Success = () => {
     return (
         <div className="container mt-5">
             <h1 className="text-center text-success">Purchase Successful!</h1>
             <p className="text-center">Thank you for your purchase. Your order is being processed and will be shipped to you soon.</p>
+            <Link to={'/'}>Continue Shopping?</Link>
         </div>
     )
 }
